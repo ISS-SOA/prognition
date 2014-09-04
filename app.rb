61d50456e69fc5bbb1c5de94072c67e0daa8597c
chenlizhan <chung1350@hotmail.com>
2014-09-05 00:25:02 +0800

    Create the result page.

--- app.rb
+++ app.rb
@@ -6,4 +6,10 @@ class CodecadetApp < Sinatra::Base
   get '/' do
     haml :home
   end
+
+  post '/result' do
+    @username = params[:username]
+    @badges_found = CodeBadges::CodecademyBadges.get_badges(@username)
+    haml :result
+  end
 end
