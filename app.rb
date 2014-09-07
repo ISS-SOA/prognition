22e7b810cfa16c33e432d40e16dba77441997195
chenlizhan <chung1350@hotmail.com>
2014-09-07 20:39:02 +0800

    Reset the route for web API

--- app.rb
+++ app.rb
@@ -1,15 +1,30 @@
 require 'sinatra/base'
 require 'codebadges'
 require 'haml'
+require 'json'
+require 'active_support/all'
 
 class CodecadetApp < Sinatra::Base
   get '/' do
+    redirect to('/cadet')
+  end
+
+  get '/cadet' do
     haml :home
   end
 
-  post '/result' do
-    @username = params[:username]
-    @badges_found = CodeBadges::CodecademyBadges.get_badges(@username)
+  get '/cadet/:username' do
+    @badges_found = CodeBadges::CodecademyBadges.get_badges(params[:username])
     haml :result
   end
+
+  post '/result' do
+    redirect to("/cadet/#{params[:username]}")
+  end
+
+  get '/api/v1/cadet/:username.json' do
+    content_type :json
+    CodeBadges::CodecademyBadges.get_badges(params[:username]).to_json
+  end
+
 end
